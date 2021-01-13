be569c99bb2ab4bb4f2f155d86391f1ab0964967
by-miwa <miwa.boardgame@gmail.com>
2021-01-13 13:48:03 +0900

    76. 画像をアップロードできるようにしよう②

--- join/index.php
+++ join/index.php
@@ -14,14 +14,24 @@ session_start();
 	if ($_POST['password'] === '') {
 		$error['password'] = 'blank';
 	}
+	$fileName = $_FILES['image']['name'];
+	if (!empty($fileName)) {
+		$ext = substr($fileName, -3);
+		if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png') {
+				$error['image'] = 'type';
+		}
+	}
 
 	if (empty($error)) {
+		$image = date('YmdHis') . $_FILES['image']['name'];
+		move_uploaded_file($_FILES['image']
+		['tmp_name'], '../member_picture/' .$image);
 		$_SESSION['join'] = $_POST;
+		$_SESSION['join']['image'] = $image;
 		header('Location: check.php');
 		exit();
 	}
 }
-
 if ($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])) {
 		$_POST = $_SESSION['join'];
 }
@@ -58,8 +68,7 @@ if ($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])) {
         	<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>" />
 					<?php if (@$error['email'] === 'blank'): ?>
 					<p class="error">* メールアドレスを入力してください</p>
-					<?php endif; ?>
-					
+					<?php endif; ?>		
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
         	<input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'],ENT_QUOTES)); ?>" />
@@ -73,6 +82,14 @@ if ($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])) {
 		<dt>写真など</dt>
 		<dd>
         	<input type="file" name="image" size="35" value="test"  />
+					<?php if (@$error['image'] === 'type'): ?>
+					<p class="error">* 写真などは[.gif]または[.jpg][.png]の画像を指定してください</p>
+					<?php endif; ?>
+					<?php if (!empty($error)): ?>
+					<p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
+					<?php endif; ?>
+
+
         </dd>
 	</dl>
 	<div><input type="submit" value="入力内容を確認する" /></div>
